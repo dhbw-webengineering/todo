6c2532ad0b4133b922e3985fa6b946ecfe672341
JaskerX <jaskerlp@gmx.de>
2025-01-24 19:48:00 +0100

    test 1

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            test 1
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
