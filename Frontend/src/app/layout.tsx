2eafffdee8d0eacd18ece5fc7d7e1b8655b11e2e
JaskerX <jaskerlp@gmx.de>
2025-02-13 09:26:43 +0100

    added navbar and test page

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Navbar/>
+        <div style={{width: "100%", height: "100%", paddingLeft: "300px"}}>
+          {children}
+        </div>
       </body>
     </html>
   );
