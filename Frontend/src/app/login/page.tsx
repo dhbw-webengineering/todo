75e4b7933e57ea17a8105d69e5152c705f5469cf
Jusitn <justinschuler16@gmail.com>
2025-03-09 21:22:46 +0100

    add handle login | password mail check | error message

--- Frontend/src/app/login/page.tsx
+++ Frontend/src/app/login/page.tsx
@@ -1,35 +1,76 @@
-import React from 'react'
+'use client'
 
-import Form from "next/form";
-import Image from 'next/image';
-
-import styles from './page.module.css';
+import React, { FormEvent, useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Login() {
+  const [error, setError] = useState<string | null>(null);
 
-  function logIn() {
-    //hier anfrage ans Backend?
-    var check: String = "admin";
-    if (check == "admin") {
-      console.log('logged in')
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+
+
+    e.preventDefault(); 
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+    
+    //API request...
+    if (email !="admin@todo" || password != "123" ) {
+      setError("wrong email or password");
+      return;
     }
 
-    return "/"
-  }
+    setError(null)
+    console.log("Logged in as admin");
+    window.location.href = "/"
+  };
 
   return (
-
     <div className={styles.loginForm}>
+      <form onSubmit={handleLogin}>
+        <Image 
+        className={styles.fImage}
+          src="/user.svg"
+          alt="Logo"
+          width={70}
+          height={70}
+        />
+        <br />
+        <input
+          className={styles.fText}
+          name="email"
+          type="email"
+          placeholder="mail"
+          required
+        />
+        <br />
+        <input
+          className={styles.fText}
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+        />
+        <br />
+        <button className={styles.fButton} type="submit">
+          Log in
+        </button>
+        <button
+          className={styles.fButton}
+          type="button"
+          onClick={() => (window.location.href = "/signup")}
+        >
+          Sign up
+        </button>
+        <br />
+      {error && <p className={styles.errorMessage}>{error}</p>} 
 
-      <Form action="/startseite">
-        <Image className={styles.fImage} src="/user.svg" alt="Logo" width={70} height={70} /><br />
-        {/* On submission, the input value will be appended to the URL, e.g. /search?user=abc&passwort=abc123 */}
-
-        <input className={styles.fText}/*name="email"*/ placeholder="email" /><br />
-        <input className={styles.fText}/*name="password"*/ type="password" placeholder="password" /> <br />
-        <button className={styles.fButton} formAction={logIn()}/*type="submit"*/>log in</button>
-        <button className={styles.fButton} formAction="/signup">sign up</button>
-      </Form>
-    </div>
-  )
+      </form>
+      </div>
+  );
 }
