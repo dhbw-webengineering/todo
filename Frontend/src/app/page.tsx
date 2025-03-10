7ecac6f1e9f1f6af2d2dc11d5c6c317985f3f466
JaskerX <jaskerlp@gmx.de>
2025-03-10 23:11:18 +0100

    added content to overview page

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,13 +1,31 @@
+import type { Metadata } from "next";
+import styles from "./page.module.css";
 
-// import Image from "next/image";
-// import styles from "./page.module.css";
+import TasksContainer from "./components/tasks/TasksContainer";
+
+
+export const metadata: Metadata = {
+  title: "Übersicht - Todo App"
+};
 
 export default function Home() {
 
-
+  const query = "http://localhost:3001/api/entry/list";
 
   return (
-  <h1>Alle Aufgaben</h1>
-  
+    <div className={styles.container}>
+    <div className={styles.titleContainer}>
+      <h1 className={styles.title}>Übersicht</h1>
+    </div>
+
+    <h3 className={styles.timeDisplay}>Heute</h3>
+    <TasksContainer query={query}/>
+    
+    <h3 className={styles.timeDisplay}>Morgen</h3>
+    <TasksContainer query={query}/>
+    
+    <h3 className={styles.timeDisplay}>Nächste 3 Tage</h3>
+    <TasksContainer query={query}/>
+  </div>
   );
   }
