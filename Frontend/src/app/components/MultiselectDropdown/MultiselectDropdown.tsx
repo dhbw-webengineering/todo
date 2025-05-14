f6e41021187e9e6a839cade76ca895e9c58602de
Jusitn <justinschuler16@gmail.com>
2025-05-14 14:54:59 +0200

    add component MultipleSelectDropdown and integrated them in page tasks

---
+++ Frontend/src/app/components/MultiselectDropdown/MultiselectDropdown.tsx
@@ -0,0 +1,74 @@
+"use client"
+import React, { useState } from "react";
+import Select, { MultiValue, StylesConfig } from "react-select";
+
+type OptionType = {
+  value: string;
+  label: string;
+};
+
+
+
+type MultiSelectDropdownProps = {
+  options: OptionType[];
+  placeholder?: string;
+};
+
+const customStyles: StylesConfig<OptionType, true> = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: '#000',
+      borderColor: '#24a0ed',
+      
+      width: 200,
+      minHeight: 35,
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      fontSize: 14,
+      color: "white",
+      backgroundColor: state.isSelected ? '#24a0ed' : state.isFocused ? 'rgb(54, 54, 54)' : 'black',
+      padding: 12,
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: '#000',
+      width: 200,
+    }),
+    multiValue: (provided) => ({
+      ...provided,
+      backgroundColor: "#24a0ed",
+      color: "#000",
+    }),
+    multiValueLabel: (provided) => ({
+      ...provided,
+      color: "#000",
+    }),
+    multiValueRemove: (provided) => ({
+      ...provided,
+      color: "#000",
+      ':hover': {
+        backgroundColor: "rgb(54, 54, 54)",
+        color: "white",
+      },
+    }),
+  };
+
+
+export default function MultiSelectDropdown({ options, placeholder }: MultiSelectDropdownProps) {
+  
+  const [selectedOptions, setSelectedOptions] = useState<MultiValue<OptionType>>([]);
+  
+  
+
+  return (
+    <Select<OptionType, true>
+      isMulti
+      options={options}
+      value={selectedOptions}
+      onChange={setSelectedOptions}
+      placeholder={placeholder}
+      styles={customStyles}
+    />
+  );
+}
--- Frontend/src/app/tasks/page.tsx
+++ Frontend/src/app/tasks/page.tsx
@@ -5,12 +5,30 @@ import styles from "./page.module.css";
 import moment from 'moment';
 import Link from 'next/link';
 import TasksContainer from '../components/tasks/TasksContainer';
+import MultiSelectDropdown from "../components/MultiselectDropdown/MultiselectDropdown";
 
 moment.locale("de")
 
 export const metadata: Metadata = {
   title: "Alle Aufgaben - Todo App"
 };
+
+//beispieldaten für Dropdowns
+const kategorieOptionen1 = [
+  { value: "Kat1A", label: "Kategorie 1A" },
+  { value: "Kat1B", label: "Kategorie 1B" }
+];
+
+const kategorieOptionen2 = [
+  { value: "Kat2A", label: "Kategorie 2A" },
+  { value: "Kat2B", label: "Kategorie 2B" }
+];
+
+const kategorieOptionen3 = [
+  { value: "Kat3A", label: "Kategorie 3A" },
+  { value: "Kat3B", label: "Kategorie 3B" }
+];
+
 
 export default function Home() {
   const query = "http://localhost:3001/api/entry/list";
@@ -20,6 +38,15 @@ export default function Home() {
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Alle Aufgaben</h1>
         <Link href="/create"><button><Image src="card.svg" alt="" width={20} height={20} /><span>neue Aufgabe erstellen</span></button></Link>
+      </div>
+      <div className={styles.menuBar}>
+        
+          <MultiSelectDropdown options={kategorieOptionen1} placeholder="Kategorien"/>
+    
+          <MultiSelectDropdown options={kategorieOptionen2} placeholder="Tags"/>
+    
+          <MultiSelectDropdown options={kategorieOptionen3} placeholder="Zeitraum"/>
+        
       </div>
       <TasksContainer query={query}/>
     </div>
